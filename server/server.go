256aee716e7fb00be63038a24cee53a2c7d30932
bannzai <kingkong999yhirose@gmail.com>
2018-11-28 01:37:13 +0900

    :shell: gqlgen init

---
+++ server/server.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	log "log"
+	http "net/http"
+	os "os"
+
+	handler "github.com/99designs/gqlgen/handler"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	http.Handle("/", handler.Playground("GraphQL playground", "/query"))
+	http.Handle("/query", handler.GraphQL(main.NewExecutableSchema(main.Config{Resolvers: &main.Resolver{}})))
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
